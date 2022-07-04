7c606e5d1a279920368386e894a7ae47c059f28c
Luior <95830293+Luior@users.noreply.github.com>
2022-07-04 19:03:09 -0300

    Delete Etapa 1 directory

--- Etapa 1/js/main.js
+++
@@ -1,6 +0,0 @@
-import dados from "./banco/bancoDados.js";
-import functions from "./banco/functions.js";
-
-if(!(localStorage.getItem('MangAgenda:mangas'))) {functions.load(dados)}
-
-functions.readAll().map((card) => functions.criar_new_Card(card))
